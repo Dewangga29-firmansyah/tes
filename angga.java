cf5b96dff334b9baffe868c7d55e28f710ce788a
Dewangga29-firmansyah <dewangga_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:15:41 +0700

    Add files via upload

---
+++ angga.java
@@ -0,0 +1,6 @@
+publik class Tes {
+    publik statis void main(String[] agrs) {
+String nama = "Adi"
+String kelas = "X RPL 5" ;
+System.out.printin("Halo,Nama saya" + Angga + "Dari kelas"+RPL5);
+    }
